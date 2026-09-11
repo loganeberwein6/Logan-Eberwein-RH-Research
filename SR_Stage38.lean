009a00e35b0eebfade15e0a2dd18ed972469d7a4
welllensllc-dotcom <welllensllc@gmail.com>
2026-09-10 22:36:11 -0500

    Stage 38: define threshold Rees matrix

--- SR_Stage38.lean
+++ SR_Stage38.lean
@@ -6,6 +6,18 @@ namespace SR
 def SR_quotient_count (X : Nat) : Nat :=
   ((completeSupport X).toFinset.image (fun m => (X - 1) / m)).card
 
+def SR_rees_matrix (X : Nat) : Matrix (Fin (X - 2)) (Fin (X - 2)) ℚ :=
+  fun i j => if (i.1 + 2) * (j.1 + 2) < X then -1 else 1
+
+theorem SR_rees_matrix_X7_entries :
+    SR_rees_matrix 7 = !![ -1, -1, 1, 1, 1;
+                            -1, 1, 1, 1, 1;
+                             1, 1, 1, 1, 1;
+                             1, 1, 1, 1, 1;
+                             1, 1, 1, 1, 1 ] := by
+  funext i j
+  fin_cases i <;> fin_cases j <;> norm_num [SR_rees_matrix]
+
 theorem SR_quotient_count_X6 : SR_quotient_count 6 = 2 := by
   native_decide
 
