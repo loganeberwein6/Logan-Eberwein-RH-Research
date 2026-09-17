ab2ff406bea85ddcafcfaf7e79d515c63904ab31
welllensllc-dotcom <welllensllc@gmail.com>
2026-09-17 13:46:08 -0500

    Clean lakefile to 7 modules; update commit reference in paper

--- lakefile.lean
+++ lakefile.lean
@@ -7,152 +7,20 @@ require mathlib from git
 package «CodexRH» where
   -- Foundation library for the Selberg-Rees arithmetic Hodge program.
 
-lean_lib SR_Primitives where
-  roots := #[`core.SR_Primitives]
-
-lean_lib SR_TransferLemma where
-  roots := #[`SR_TransferLemma]
-
-lean_lib SR_MultiChannel where
-  roots := #[`SR_MultiChannel]
-
-lean_lib SR_ThetaKernel where
-  roots := #[`spectral.SR_ThetaKernel]
-
-lean_lib SR_ZeroTransfer where
-  roots := #[`spectral.SR_ZeroTransfer]
-
-lean_lib SR_SpectralBridge where
-  roots := #[`SR_SpectralBridge]
-
-lean_lib SR_Correlation where
-  roots := #[`SR_Correlation]
-
-lean_lib SR_KnowledgeBank where
-  roots := #[`SR_KnowledgeBank]
-
-lean_lib SR_Carrier where
-  roots := #[`SR_Carrier]
-
-lean_lib SR_Multiplication where
-  roots := #[`SR_Multiplication]
-
-lean_lib SR_Dagger where
-  roots := #[`spectral.SR_Dagger]
-
-lean_lib SR_NoCommutativity where
-  roots := #[`SR_NoCommutativity]
-
-lean_lib SR_Rees where
-  roots := #[`core.SR_Rees]
-
-lean_lib SR_GNS where
-  roots := #[`spectral.SR_GNS]
-
-lean_lib SR_Underdetermination where
-  roots := #[`SR_Underdetermination]
-
-lean_lib SR_Stage8 where
-  roots := #[`SR_Stage8]
-
-lean_lib SR_Valuation where
-  roots := #[`SR_Valuation]
-
-lean_lib SR_Stage9 where
-  roots := #[`SR_Stage9]
-
-lean_lib SR_PoincareDuality where
-  roots := #[`SR_PoincareDuality]
-
-lean_lib SR_LefschetzDiagnosis where
-  roots := #[`SR_LefschetzDiagnosis]
-
-lean_lib SR_Lefschetz where
-  roots := #[`SR_Lefschetz]
-
-lean_lib SR_HermitianForm where
-  roots := #[`spectral.SR_HermitianForm]
-
-lean_lib SR_ISR where
-  roots := #[`SR_ISR]
-
-lean_lib SR_ISR_Full where
-  roots := #[`SR_ISR_Full]
-
-lean_lib SR_Signature where
-  roots := #[`SR_Signature]
-
-lean_lib SR_Flow where
-  roots := #[`weil.SR_Flow]
-
-lean_lib SR_Balance where
-  roots := #[`SR_Balance]
-
-lean_lib SR_Balance_Comparison where
-  roots := #[`SR_Balance_Comparison]
-
-lean_lib SR_Primorial where
-  roots := #[`SR_Primorial]
-
-lean_lib SR_SignatureBound where
-  roots := #[`SR_SignatureBound]
-
-lean_lib SR_UpperBound where
-  roots := #[`SR_UpperBound]
-
-lean_lib SR_Density where
-  roots := #[`SR_Density]
-
-lean_lib SR_Boundary where
-  roots := #[`SR_Boundary]
-
-lean_lib SR_Mollifier where
-  roots := #[`SR_Mollifier]
-
-lean_lib SR_Bridge where
-  roots := #[`weil.SR_Bridge]
-
-lean_lib SR_Stage27 where
-  roots := #[`SR_Stage27]
-
-lean_lib SR_Stage28 where
-  roots := #[`SR_Stage28]
+lean_lib SR_Stage38 where
+  roots := #[`core.SR_Stage38]
 
 lean_lib SR_WeilForm where
   roots := #[`weil.SR_WeilForm]
 
-lean_lib SR_WEntropy where
-  roots := #[`broken.SR_WEntropy]
+lean_lib SR_Dagger where
+  roots := #[`spectral.SR_Dagger]
 
-lean_lib SR_Stage29 where
-  roots := #[`SR_Stage29]
+lean_lib SR_GNS where
+  roots := #[`spectral.SR_GNS]
 
-lean_lib SR_Stage30_Bound where
-  roots := #[`SR_Stage30_Bound]
-
-lean_lib SR_Stage30 where
-  roots := #[`SR_Stage30]
-
-lean_lib SR_Stage31 where
-  roots := #[`SR_Stage31]
-
-lean_lib SR_Stage33 where
-  roots := #[`SR_Stage33]
-
-lean_lib SR_Stage34 where
-  roots := #[`SR_Stage34]
-
-lean_lib SR_Stage35 where
-  roots := #[`SR_Stage35]
-
-lean_lib SR_Stage36 where
-  roots := #[`SR_Stage36]
-
-lean_lib SR_Stage37_Clean where
-  roots := #[`core.SR_Stage37_Clean]
-
-lean_lib SR_Stage38 where
-  roots := #[`core.SR_Stage38]
+lean_lib SR_ZeroTransfer where
+  roots := #[`spectral.SR_ZeroTransfer]
 
 lean_lib RH_Summary where
   roots := #[`ledger.RH_Summary]
